43d83d9a4db2931378dca077f8969c37fcfe4bee
raphaeltcf <raphaeltcferreira@gmail.com>
2025-11-19 13:39:37 -0300

    Remove Address model, serializer, service methods, and viewset from the users app, streamlining user management functionality.

--- users/serializer.py
+++ users/serializer.py
@@ -6,9 +6,3 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'
         read_only_fields = ['created_at', 'updated_at']
-
-class AddressSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Address
-        fields = '__all__'
-        read_only_fields = ['created_at', 'updated_at']
